eb4280585ffbe3d3e56a11a9b8fdfd80f21d1606
DidieroSZ <octaviosz2003@hotmail.com>
2025-10-01 16:09:52 -0600

    feat(js): Curso Proyecto 15 [BBVACS-2798]

---
+++ JS/Apuntes/Arrays.js
@@ -0,0 +1,11 @@
+const categorias = [
+    { nombre: 'Playeras', cantidad: 200 },
+    { nombre: 'Camisas', cantidad: 145 },
+    { nombre: 'Sudaderas', cantidad: 982 },
+    { nombre: 'Sacos', cantidad: 732 },
+    { nombre: 'Hoodies', cantidad: 23 },
+    { nombre: 'Jeans', cantidad: 92 },
+    { nombre: 'Shorts', cantidad: 54 }
+];
+
+console.log(categorias.map());
