6140359c1f94c10c47f6a5d9c4895c24ad779c22
Didier Saucedo <octaviosz2003@hotmail.com>
2025-10-07 11:58:21 -0600

    feat(js): ADDED Lit-Vanilla [BBVACS-2795]

---
+++ CELLS/proyecto-opencells/litVanilla/src/mi-componete.js
@@ -0,0 +1,12 @@
+import { LitElement, html } from "lit-element";
+
+export class miComponente extends LitElement {
+    render(){
+        return html`
+        <p> Hola desde LitElement </p>
+        
+        `;
+    }
+}
+
+customElements.define('mi-componente', miComponente);
