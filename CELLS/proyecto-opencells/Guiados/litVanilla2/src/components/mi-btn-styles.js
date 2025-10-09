5a4373427dfce60b8a98b1a782a9ff0ccd3d7ec0
Didier Saucedo <octaviosz2003@hotmail.com>
2025-10-09 10:47:10 -0600

    fix(js): REESTRUCTURA DE CARPETAS CELLS

---
+++ CELLS/proyecto-opencells/Guiados/litVanilla2/src/components/mi-btn-styles.js
@@ -0,0 +1,20 @@
+import { css } from "lit";
+
+export default css`
+button {
+    padding: 1rem 1.5rem;
+    background-color: red;
+    color: white;
+    border: none;
+    border-radius: 50px;
+}
+
+.btnDefault{
+    background-color: green;
+}
+.btnWarning{
+    background-color: yellow;
+}
+
+
+`;
